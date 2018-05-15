a39032643dac310377151ff660a358cb78071405
Huizerd <jessehagenaars@gmail.com>
2018-05-15 14:28:19 +0200

    Adjusted files for cloud

--- run_train_cloud.sh
+++ run_train_cloud.sh
@@ -2,7 +2,7 @@
 python monodepth_main.py --mode train \
 --model_name 1day_cloud \
 --data_path ~/KITTI/ \
---filenames_file utils/filenames/kitti_train_files_random_png.txt \
+--filenames_file utils/filenames/2011_09_29_files.txt \
 --output_directory disparities/ \
 --log_directory logs/ \
 --checkpoint_path models/1day_cloud \
