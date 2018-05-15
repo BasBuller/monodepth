65546a7678a3e90bf65601a8d75ad67042a43a01
Huizerd <jessehagenaars@gmail.com>
2018-05-15 16:47:00 +0200

    Model name cloud

--- run_main_cloud.sh
+++ run_main_cloud.sh
@@ -4,5 +4,5 @@ python monodepth_main.py --mode test \
 --filenames_file utils/filenames/kitti_stereo_2015_test_files_png.txt \
 --output_directory disparities/ \
 --log_directory logs/ \
---checkpoint_path models/1day_cloud \
+--checkpoint_path models/1day_cloud_950 \
 --full_summary
