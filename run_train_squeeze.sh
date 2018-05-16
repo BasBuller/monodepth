ad2d6371361c6611337babc7347972e2d8d780a8
BasBuller <bas.buller@gmail.com>
2018-05-16 15:36:06 +0200

    made run train work by adding correct args

--- run_train_squeeze.sh
+++ run_train_squeeze.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 python monodepth_main.py --mode train \
 --model_name squeeze_net \
---data_path /home/bas/Documents/monodepth_data/stereo_2015/ \
+--encoder squeeze_net \
+--data_path data/stereo_2015/ \
 --filenames_file utils/filenames/kitti_stereo_2015_test_files_png.txt \
 --output_directory disparities/ \
 --log_directory logs/ \
---checkpoint_path models/squeeze_net \
---encoder squeeze_net \
---full_summary
+--num_epochs 1 \
+--batch_size 20
+# --checkpoint_path models/squeeze_net \
+# --input_height 375 \
+# --input_width 1242 
+# --full_summary
