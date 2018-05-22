376ba3cdf5763960e27b83e81f8b4869305424e3
BasBuller <bas.buller@gmail.com>
2018-05-22 11:55:41 +0200

    fixing saving permission error

--- run_train_squeeze_cloud.sh
+++ run_train_squeeze_cloud.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 python monodepth_main.py --mode train \
---model_name vgg \
---encoder vgg \
+--model_name squeeze_net \
+--encoder squeeze_net \
 --data_path /home/shared/KITTI/stereo_2015/ \
 --filenames_file utils/filenames/kitti_stereo_2015_test_files_png.txt \
 --output_directory disparities/ \
