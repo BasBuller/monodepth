097f3a6fff63993e4933ee0dd612433087925159
BasBuller <bas.buller@gmail.com>
2018-05-22 13:16:25 +0200

    trying retraining

--- run_train_squeeze_cloud.sh
+++ run_train_squeeze_cloud.sh
@@ -5,7 +5,8 @@ python monodepth_main.py --mode train \
 --data_path /home/shared/KITTI/stereo_2015/ \
 --filenames_file utils/filenames/kitti_stereo_2015_test_files_png.txt \
 --log_directory logs/ \
---checkpoint_path models/squeeze_net \
+--checkpoint_path models/squeeze_net/model-squeeze_net \
+--retrain \
 --num_epochs 1 \
 --batch_size 20 \
 
