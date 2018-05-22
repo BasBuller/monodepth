27e221b730a77d75bbc8bd1256465b6a39ef16c2
Huizerd <jessehagenaars@gmail.com>
2018-05-22 09:13:18 +0200

    Added pruning hparams

--- run_train_pruning_cloud.sh
+++ run_train_pruning_cloud.sh
@@ -4,6 +4,7 @@ python monodepth_main.py --mode train \
 --data_path /home/shared/KITTI/ \
 --filenames_file utils/filenames/2011_09_29_files.txt \
 --use_prunable \
+--pruning_hparams target_sparsity=0.7 \
 --output_directory disparities/ \
 --log_directory logs/ \
 --checkpoint_path /home/shared/models/model_city2kitti \
