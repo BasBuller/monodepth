0b357d62a52997096897770125f34a55c30f9409
BasBuller <bas.buller@gmail.com>
2018-05-22 09:55:09 +0200

    squeeze cloud sh file

---
+++ run_train_squeeze_cloud.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+python monodepth_main.py --mode train \
+--model_name squeeze_net \
+--encoder squeeze_net \
+--data_path /home/shared/KITTI/ \
+--filenames_file utils/filenames/kitti_stereo_2015_test_files_png.txt \
+--output_directory disparities/ \
+--log_directory logs/ \
+--num_epochs 1 \
+--batch_size 20 \
+--full_summary
