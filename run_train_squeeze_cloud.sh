bb80388062b2e03cdddc639f9c8bc6ff7acac620
BasBuller <bas.buller@gmail.com>
2018-05-22 08:03:03 +0000

    error fix

--- run_train_squeeze_cloud.sh
+++ run_train_squeeze_cloud.sh
@@ -2,7 +2,7 @@
 python monodepth_main.py --mode train \
 --model_name squeeze_net \
 --encoder squeeze_net \
---data_path /home/shared/KITTI/ \
+--data_path /home/shared/KITTI/stereo_2015/ \
 --filenames_file utils/filenames/kitti_stereo_2015_test_files_png.txt \
 --output_directory disparities/ \
 --log_directory logs/ \
