a6dbf53fe8fe44e36504d995cf0069d249c1ec77
Huizerd <jessehagenaars@gmail.com>
2018-05-29 11:46:49 +0000

    Cloud config

--- run_train_pruning_cloud.sh
+++ run_train_pruning_cloud.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 python monodepth_main.py --mode train \
---model_name insert_model_name \
+--model_name insert_model_name_095 \
 --data_path /home/shared/KITTI/ \
 --filenames_file utils/filenames/2011_09_29_files.txt \
 --use_prunable \
+--pruning_hparams target_sparsity=0.5 \
 --output_directory disparities/ \
 --log_directory logs/ \
 --checkpoint_path /home/shared/models/model_city2kitti \
