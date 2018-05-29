68008ac6afc6aacf3ddc024c5695e4807ad0f403
BasBuller <bas.buller@gmail.com>
2018-05-29 12:47:03 +0200

    delayed pool one time

--- run_train_squeeze_cloud.sh
+++ run_train_squeeze_cloud.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 python monodepth_main.py --mode train \
---model_name squeeze_net \
---encoder squeeze_net \
+--model_name delayed_pool_one \
+--encoder delayed_pool_one \
 --data_path /home/shared/KITTI/stereo_2015/ \
 --filenames_file utils/filenames/kitti_stereo_2015_test_files_png.txt \
 --log_directory logs/ \
