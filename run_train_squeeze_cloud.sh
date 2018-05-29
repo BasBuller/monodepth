b14da28c456af4608dc5d25b91211027d7adee2e
BasBuller <bas.buller@gmail.com>
2018-05-29 12:55:19 +0200

    small decoder one time

--- run_train_squeeze_cloud.sh
+++ run_train_squeeze_cloud.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 python monodepth_main.py --mode train \
---model_name delayed_pool_two \
---encoder delayed_pool_two \
+--model_name small_decoder_one \
+--encoder small_decoder_one \
 --data_path /home/shared/KITTI/stereo_2015/ \
 --filenames_file utils/filenames/kitti_stereo_2015_test_files_png.txt \
 --log_directory logs/ \
