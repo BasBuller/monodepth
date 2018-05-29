1c8c8c819f08d419f5f080fc859cf28e3e0419cf
BasBuller <bas.buller@gmail.com>
2018-05-29 11:36:59 +0200

    all models can be trained

--- run_train_delayed_pool.sh
+++ run_train_delayed_pool.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 python monodepth_main.py --mode train \
---model_name delayed_pool \
---encoder delayed_pool \
+--model_name delayed_pool_two \
+--encoder delayed_pool_two \
 --data_path data/stereo_2015/ \
 --filenames_file utils/filenames/kitti_stereo_2015_test_files_png.txt \
 --log_directory logs/ \
--- run_train_small_decoder.sh
+++ run_train_small_decoder.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 python monodepth_main.py --mode train \
---model_name small_decoder \
---encoder small_decoder \
+--model_name small_decoder_two \
+--encoder small_decoder_two \
 --data_path data/stereo_2015/ \
 --filenames_file utils/filenames/kitti_stereo_2015_test_files_png.txt \
 --log_directory logs/ \
