8267a3b2f5597e546979a415b30c70357dcc46e6
R1ckF <feith.rick@gmail.com>
2018-06-03 19:09:27 +0000

    version that was trained on 3 jun

--- run_train_squeeze_cloud.sh
+++ run_train_squeeze_cloud.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 python monodepth_main.py --mode train \
---model_name small_decoder_two \
---encoder small_decoder_two \
---data_path /home/shared/KITTI/stereo_2015/ \
---filenames_file utils/filenames/kitti_stereo_2015_test_files_png.txt \
+--model_name squeeze_net \
+--encoder squeeze_net \
+--data_path /home/shared/data/KITTI/ \
+--filenames_file utils/filenames/kitti_train_files.txt \
 --log_directory logs/ \
---num_epochs 1 \
---batch_size 10
+--num_epochs 50 \
+--batch_size 8
 # --checkpoint_path models/squeeze_net/model-squeeze_net \
