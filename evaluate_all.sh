0205fd1d6ab37900aa6d85a958d45ca50e8ef35e
Tom van Dijk <tomvand@users.noreply.github.com>
2018-06-12 09:32:11 +0000

    WIP bugfixes

--- evaluate_all.sh
+++ evaluate_all.sh
@@ -12,25 +12,25 @@ TRAIN_FILENAMES_FILE=utils/filenames/kitti_train_files_png.txt
 #TRAIN_DATA_PATH=/home/shared/KITTI/
 TRAIN_DATA_PATH=/home/shared/data/KITTI/
 
-#TEST_DATA_PATH=/home/shared/KITTI/stereo_2015/
-TEST_DATA_PATH=/home/shared/data/KITTI/
-#TEST_FILENAMES_FILE=utils/filenames/kitti_stereo_2015_test_files_png.txt
-TEST_FILENAMES_FILE=utils/filenames/kitti_test_files_png.txt
+TEST_DATA_PATH=/home/shared/data/KITTI/stereo_2015/
+#TEST_DATA_PATH=/home/shared/data/KITTI/
+TEST_FILENAMES_FILE=utils/filenames/kitti_stereo_2015_test_files_png.txt
+#TEST_FILENAMES_FILE=utils/filenames/kitti_test_files_png.txt
 
 RESULTS_PATH=/home/shared/results/results.pickle
 
 #for LAYERS in 6 5 4
-for LAYERS in 5 4
+for LAYERS in 7 6 5
 do
-    echo "Training ${LAYERS} layers..."
-    python monodepth_main.py --mode train \
-    --model_name ${MODEL_NAME}_${LAYERS}layers \
-    --data_path  $TRAIN_DATA_PATH \
-    --filenames_file $TRAIN_FILENAMES_FILE \
-    --output_directory disparities/ \
-    --log_directory $LOG_DIRECTORY \
-    --full_summary \
-    --num_layers ${LAYERS}
+    #echo "Training ${LAYERS} layers..."
+    #python monodepth_main.py --mode train \
+    #--model_name ${MODEL_NAME}_${LAYERS}layers \
+    #--data_path  $TRAIN_DATA_PATH \
+    #--filenames_file $TRAIN_FILENAMES_FILE \
+    #--output_directory disparities/ \
+    #--log_directory $LOG_DIRECTORY \
+    #--full_summary \
+    #--num_layers ${LAYERS}
 
     echo "Running ${LAYERS} layers..."
     python monodepth_main.py --mode test \
@@ -38,8 +38,7 @@ do
     --filenames_file $TEST_FILENAMES_FILE \
     --output_directory disparities/ \
     --log_directory $LOG_DIRECTORY \
-    --checkpoint_path ${LOG_DIRECTORY}/${MODEL_NAME}_${LAYERS}layers/model-950 \
-    --full_summary \
+    --checkpoint_path ${LOG_DIRECTORY}/${MODEL_NAME}_${LAYERS}layers/model-181250 \
     --num_layers ${LAYERS}
 
     echo "Evaluating ${LAYERS} layers..."
