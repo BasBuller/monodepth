ff87879f9427260ff0d9e92575b6ed8cc8797bc8
Huizerd <jessehagenaars@gmail.com>
2018-06-13 08:27:24 +0200

    Added new file for only testing and eval

--- run_test_all.sh
+++ run_test_all.sh
@@ -8,10 +8,6 @@ MODEL_NAME=pruning
 # For summaries, etc.
 LOG_DIRECTORY=logs/
 
-# Training
-TRAIN_DATA_PATH=/home/shared/data/KITTI/
-TRAIN_FILENAMES_FILE=utils/filenames/kitti_train_files_26_png.txt
-
 # Testing
 TEST_DATA_PATH=/home/shared/data/KITTI/
 TEST_FILENAMES_FILE=utils/filenames/kitti_test_files_png.txt
@@ -25,22 +21,8 @@ DISP_PATH=disparities/
 # For pickle
 RESULTS_PATH=logs/
 
-# Checkpoint model
-CHECKPOINT_PATH=models/model_kitti
-
 for SPARSITY in 0.3 0.5 0.7 0.9
 do
-    echo "Training ${SPARSITY} sparsity..."
-    python monodepth_main.py --mode train \
-    --model_name ${MODEL_NAME}_${SPARSITY} \
-    --data_path ${TRAIN_DATA_PATH} \
-    --filenames_file ${TRAIN_FILENAMES_FILE} \
-    --use_prunable \
-    --pruning_hparams "target_sparsity=${SPARSITY}, sparsity_function_begin_step=5000, sparsity_function_end_step=15000" \
-    --output_directory ${DISP_PATH} \
-    --log_directory ${LOG_DIRECTORY} \
-    --checkpoint_path ${CHECKPOINT_PATH} \
-    --retrain
 
     echo "Testing ${SPARSITY} sparsity..."
     python monodepth_main.py --mode test \
